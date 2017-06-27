5f02df82b4909344eba2e4811d300410928dc05d
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-27 14:21:25 -0600

    add unit spec for InvoiceItem model

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to an item and an invoice" do
+    item = Item.create(name: "Item Name", description: "Item Description")
+    invoice = Invoice.create(status: "shipped")
+    invoice_item = InvoiceItem.create(
+                                      item_id: item.id,
+                                      invoice_id: invoice.id,
+                                      quantity: 2,
+                                      unit_price: item.unit_price
+                                      )
+    expect(invoice_item).to be_valid
+    expect(invoice_item.unit_price).to eq(item.unit_price)
+  end
 end
