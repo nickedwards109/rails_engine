b9d9c4229abd4a7d92b39a06b389a47718f78f52
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-26 21:14:56 -0600

    specify get item single finder endpoints
    specify an endpoint for finding an item by a certain attribute, using the syntax GET /api/v1/merchants/find?attribute_name=attribute_value

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -14,6 +14,7 @@ describe "Items API" do
     item = items.first
     expect(item).to have_key("name")
     expect(item).to have_key("description")
+    expect(item).to have_key("unit_price")
   end
 
   it "sends a single item" do
@@ -26,5 +27,49 @@ describe "Items API" do
     raw_item = JSON.parse(response.body)
     expect(raw_item["name"]).to eq(item.name)
     expect(raw_item["description"]).to eq(item.description)
+    expect(raw_item["unit_price"]).to eq(item.unit_price)
+  end
+
+  it "sends an item selected by id, name, description, unit_price, created_at, or updated_at" do
+    item = create(:item)
+
+    get "/api/v1/items/find?id=#{item.id}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["id"]).to eq(item.id)
+
+    get "/api/v1/items/find?name=#{item.name}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["name"]).to eq(item.name)
+
+    get "/api/v1/items/find?description=#{item.description}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["description"]).to eq(item.description)
+
+    get "/api/v1/items/find?unit_price=#{item.unit_price}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["unit_price"]).to eq(item.unit_price)
+
+    get "/api/v1/items/find?created_at=#{item.created_at}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["created_at"]).to eq(item.created_at)
+
+    get "/api/v1/items/find?updated_at=#{item.updated_at}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["updated_at"]).to eq(item.updated_at)
+
+    # check for case insensitivity on one attribute.
+    # if this fails, it signals that case insensitivity probably doesn't work
+    # on the other attributes either.
+    # no need to duplicate this for every attribute and slow down the tests.
+    get "/api/v1/items/find?name=#{item.name.upcase}"
+    expect(response).to be_success
+    raw_item = JSON.parse(response.body)
+    expect(raw_item["name"]).to eq(item.name)
   end
 end
