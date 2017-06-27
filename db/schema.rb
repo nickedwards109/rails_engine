364629c69fe56747422a2f39f940769f70eebc73
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-26 21:13:56 -0600

    add unit price attribute to item model
    save the unit price as a string, since the project requirements specify it as a string.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626231457) do
+ActiveRecord::Schema.define(version: 20170627031218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170626231457) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "unit_price"
   end
 
 end
