4a9b1957e63e6d9b47a10b96f49320b145fbd72d
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-28 10:43:33 -0600

    finish adding model associations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,10 @@
 class Invoice < ApplicationRecord
+  belongs_to :merchant
+  belongs_to :customer
   has_many :transactions
   has_many :invoice_items
-  
+  has_many :items, through: :invoice_items
+
   def self.find_case_insensitive(invoice_params)
     if invoice_params[:status]
       Invoice.find_by("status ILIKE ?", invoice_params[:status])
