88c0408532a48a12b3db2c6b08ead05181a456dd
Cody <codyborders@gmail.com>
2017-06-28 09:05:26 -0600

    adds simplecov removes duplicate migration for items/merchant relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627204036) do
+ActiveRecord::Schema.define(version: 20170628002103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170627204036) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "invoice_items", force: :cascade do |t|
     t.bigint "item_id"
@@ -68,13 +69,10 @@ ActiveRecord::Schema.define(version: 20170627204036) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
-  
-  add_foreign_key "invoice_items", "invoices"
-  add_foreign_key "invoice_items", "items"
-  add_foreign_key "items", "merchants"
-
 end
