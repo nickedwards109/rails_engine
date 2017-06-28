7c870f9a9dde5803797025d3bf145810952594c0
Cody <codyborders@gmail.com>
2017-06-28 16:28:10 -0600

    adds relationship endpoint for invoices/transactions

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
         get '/:id/merchant', to: "merchant#show"
         get '/:id/items', to: "items#index"
         get '/:id/invoice_items', to: "invoice_items#index"
+        get '/:id/transactions', to: "transactions#index"
       end
 
       namespace :customers do
