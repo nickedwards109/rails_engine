c761a69ac420737c90e8150efa0a1b045b1c1556
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-06-29 09:34:04 -0600

    add unit spec for Merchant#favorite_customer

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "finds the merchant's favorite customer" do
+    good_customer = create(:customer)
+    bad_customer = create(:customer)
+    merchant = create(:merchant)
+
+    successful_transaction_1 = create(:transaction, result: "success")
+    successful_transaction_2 = create(:transaction, result: "success")
+    successful_transaction_3 = create(:transaction, result: "success")
+    unsuccessful_transaction = create(:transaction, result: "success")
+
+    successful_transaction_1.customer = good_customer
+    successful_transaction_2.customer = good_customer
+    successful_transaction_3.customer = bad_customer
+    unsuccessful_transaction.customer = bad_customer
+
+    expect(merchant.favorite_customer).to eq(good_customer)
+  end
 end
